ba5d884ebf8b6cb4c75d083f05585f86b819d87a
sebastienlavoie <slavoie@ellicom.com>
2012-10-05 16:17:54 -0400

    Batch file looks like it's working.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off 
 set file_name=%1%
-set version=CS6
-set base_path=/c/Program Files/Adobe
-set full_path=%base_path%/Adobe After Effects %version%/Scripts/User
+set version=CS5.5
+set full_path=c:\Program Files\Adobe\Adobe After Effects %version%\Support Files\Scripts\User
+
 @echo on
-copy %file_name% %full_path%/%file_name%
-echo "Successfully compiled %file_name% to %full_path%/%file_name%"
+copy "%file_name%" "%full_path%\%file_name%"
+echo "Successfully compiled %file_name% to %full_path%\%file_name%"
