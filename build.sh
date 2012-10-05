fb4a1df374e521a28a6344870d29316fd09eef32
sebastienlavoie <slavoie@ellicom.com>
2012-10-05 14:22:53 -0400

    Testing for Windows. Not yet working. Crazy UTF8 bug.

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 file_extension=$1;
 file_name=$2;
-version="CS6";
+# Usual base_path in Windows : /c/Program Files/Adobe
+# Usual base_path in OS X : /Applications
+base_path="/c/Program Files/Adobe";
+
+version="CS5.5";
 
 case "$file_name" in
 "PresetEffects.xml")
-  echo "Successfully compiled ${file_name} to /Applications/Adobe After Effects ${version}/Adobe After Effects ${version}.app/Contents/Resources/${file_name}"
-  cp $file_name "/Applications/Adobe After Effects ${version}/Adobe After Effects ${version}.app/Contents/Resources/${file_name}"
+  echo "Successfully compiled ${file_name} to ${base_path}/Adobe After Effects ${version}/Adobe After Effects ${version}.app/Contents/Resources/${file_name}"
+  cp $file_name "${base_path}/Adobe After Effects ${version}/Adobe After Effects ${version}.app/Contents/Resources/${file_name}"
   ;;
 esac
 
 case "$file_extension" in 
 "jsx")
-  echo "Successfully compiled ${file_name} to /Applications/Adobe After Effects ${version}/Scripts/User/${file_name}";
-  cp $file_name "/Applications/Adobe After Effects ${version}/Scripts/User/${file_name}"
+  echo "Successfully compiled ${file_name} to ${base_path}/Adobe After Effects ${version}/Scripts/User/${file_name}";
+  cp $file_name "${base_path}/Adobe After Effects ${version}/Scripts/User/${file_name}"
   ;;
 esac
