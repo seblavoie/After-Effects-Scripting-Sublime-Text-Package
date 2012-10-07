a5611e0e8edf4d63697020f274f2b88a0b5d4958
Sébastien Lavoie <s.lavoie.b@gmail.com>
2012-10-07 15:27:16 -0400

    Added applescript run on build in AfterEffects.

--- README.md
+++ README.md
@@ -4,8 +4,4 @@ After Effects Scripting Sublime Text 2 Package
 Sublime Text 2 package for After Effects. 
 
 ## Build System
-The build system will copy your current file in the defined path.
-
-## Todos
-- Create afterEffectScripting.tmLanguage
-- Add windows support
+The build system will copy your current jsx file in your AfterEffects script folder so you don't have to develop in that folder.
--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 file_name=$1;
+_packages=$2;
 version="CS6";
 base_path="/Applications";
 full_path="${base_path}/Adobe After Effects ${version}/Scripts/User";
 
+
+# Copying built file to script folder
+cp $file_name "${full_path}/${file_name}";
+
+# Arguments to Appllescript file not working. Using clipboard instead.
+echo "${full_path}/${file_name}" | pbcopy
+
+# Running appleScript file
+osascript ${_packages}/AfterEffects/run.scpt;
+
+# Printing happy feedback in the console
 echo "Successfully compiled ${file_name} to ${full_path}/${file_name}";
-cp $file_name "${base_path}/Adobe After Effects ${full_path}/${file_name}"
