f90cf97e45adefcf47efa9a13f92c7d2d5aeed8a
sebastienlavoie <slavoie@ellicom.com>
2012-10-11 14:37:40 -0400

    Fixed windows path problem

--- build.bat
+++ build.bat
@@ -1,9 +1,14 @@
+echo file_name=%1%
+echo file_path=%2%
+
 @echo off 
 set file_name=%1%
 set file_path=%2%
 set version=CS5.5
 set full_path=c:\Program Files\Adobe\Adobe After Effects %version%\Support Files\Scripts\User
 
+
 @echo on
-copy "%file_path%\%file_name%" "%full_path%\%file_name%"
+cd "%file_path%"
+copy "%file_name%" "%full_path%\%file_name%"
 echo "Successfully compiled %file_name% to %full_path%\%file_name%"
