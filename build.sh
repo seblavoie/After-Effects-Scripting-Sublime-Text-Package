a87ad4ffb9d49ceed03cb0b3e1b004960444d3cf
sebastienlavoie <slavoie@ellicom.com>
2012-10-24 14:46:02 -0400

    Removed subfolder var

--- build.sh
+++ build.sh
@@ -7,11 +7,8 @@ version="CS6";
 # Adobe After Effects folder location
 base_path="Applications";
 
-# Change this if you want your scripts to be in a subfolder 
-subfolder=""
-
 # Full path
-full_path="/${base_path}/Adobe After Effects ${version}/Scripts${subfolder}";
+full_path="/${base_path}/Adobe After Effects ${version}/Scripts";
 
 # Copying built file to script folder
 cp $file_name "${full_path}/${file_name}";
